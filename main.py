590abb01addc2cbb5e6d314da87bad2dd462e427
jsd115 <125307559+jsd115@users.noreply.github.com>
2024-09-10 09:37:07 -0400

    Updated charts and commented code

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
+"""
+This file contains the code to create a Dash application that displays the results of the SQL queries in a dashboard. The application contains six tabs, each displaying a different visualization of the data. The data is read from a CSV file and inserted into an in-memory SQLite database. The SQL queries are executed on the database to extract the required information for the visualizations. The visualizations are created using Plotly and displayed in the Dash application.
+"""
+
+# Import required libraries
 import sqlite3
 import pandas as pd
 import plotly.graph_objects as go
 import dash
 from dash import dcc
 from dash import html
+
+# Import SQL queries from queries.py
 from queries import *
 
-data = pd.read_csv('sql_project/digital_wallet_transactions.csv')
+# Read the data from the CSV file
+data = pd.read_csv('digital_wallet_transactions.csv')
+
+# Drop the 'idx' column from the data
 data = data.drop('idx', axis=1)
 
-# Create a connection to an SQLite database (in-memory or a file)
-conn = sqlite3.connect(':memory:')  # Use ':memory:' to create a database in RAM
+# Create a connection to an SQLite database in memory
+conn = sqlite3.connect(':memory:')
+
+# Create a cursor object using the connection
+# Cursor is used to execute SQL queries
 cursor = conn.cursor()
 
-# Create a sample table
+# Create a table in the database
+# These columns are the columns in the pandas DataFrame
+# The information is gathered from the data_exploration.ipynb file
 cursor.execute('''
 CREATE TABLE digital_wallet_transaction (
     transaction_id TEXT PRIMARY KEY,
@@ -34,21 +49,25 @@ CREATE TABLE digital_wallet_transaction (
 )
 ''')
 
-# Insert sample data
+# Insert data into the table from the pandas DataFrame
+# Create a for loop to iterate over the rows of the DataFrame
 for index, row in data.iterrows():
+    # Insert each row into the table
     cursor.execute('''
     INSERT INTO digital_wallet_transaction (
         transaction_id, user_id, transaction_date, product_category, product_name, 
         merchant_name, product_amount, transaction_fee, cashback, loyalty_points, 
         payment_method, transaction_status, merchant_id, device_type, location
     ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ''', (
+    ''', 
+    # Use the values from the row to insert into the table
+    (
         row['transaction_id'], row['user_id'], row['transaction_date'], row['product_category'], row['product_name'], 
         row['merchant_name'], row['product_amount'], row['transaction_fee'], row['cashback'], row['loyalty_points'], 
         row['payment_method'], row['transaction_status'], row['merchant_id'], row['device_type'], row['location']
     ))
 
-# Read SQL data into a pandas DataFrame
+# Read SQL data into a pandas DataFrame for each query from queries.py
 df_query_1 = pd.read_sql_query(query_1, conn)
 df_query_2 = pd.read_sql_query(query_2, conn)
 df_query_3 = pd.read_sql_query(query_3, conn)
@@ -56,20 +75,68 @@ df_query_4 = pd.read_sql_query(query_4, conn)
 df_query_5 = pd.read_sql_query(query_5, conn)
 df_query_6 = pd.read_sql_query(query_6, conn)
 
-colors = ['orange', 'blue', 'green', 'red', 'purple', 'brown', 'pink', 'yellow', 'grey']
-
 # Commit changes and close the cursor
 conn.commit()
 conn.close()
 
-# Create a Dash application
+# Define colors for the payment method plot
+colors = ['orange', 'blue', 'green', 'red', 'purple', 'brown', 'pink', 'yellow', 'grey']
+
+# Define colors for the device usage plot
+colors_device_usage = ['orange', 'blue', 'purple']
+
+# Extract unique device types
+device_types = df_query_3['device_type'].unique()
+
+# Create a list to hold the bar objects
+bars = []
+
+# Iterate over each device type and create a bar for it
+for i, device_type in enumerate(device_types):
+    filtered_df = df_query_3[df_query_3['device_type'] == device_type]
+    bars.append(
+        go.Bar(
+            x=filtered_df['location'],
+            y=filtered_df['count_device_type_by_location'],
+            name=device_type,
+            marker=dict(color=colors_device_usage[i]),
+            text=filtered_df['device_type'],
+            hoverinfo='text+y'
+        )
+    )
+
+# Extract unique payment methods
+payment_methods = df_query_5['payment_method'].unique()
+
+# Create a list to hold the bar objects
+payment_method_bars = []
+
+# Iterate over each payment method and create a bar for it
+for i, payment_method in enumerate(payment_methods):
+    payment_method_filtered_df = df_query_5[df_query_5['payment_method'] == payment_method]
+    payment_method_bars.append(
+        go.Bar(
+            x=payment_method_filtered_df['location'],
+            y=payment_method_filtered_df['count_payment_method_by_location'],
+            name=payment_method,
+            marker=dict(color=colors[i % len(colors)]),  # Cycle through colors if there are more payment methods than colors
+            text=payment_method_filtered_df['payment_method'],
+            hoverinfo='text+y'
+        )
+    )
+
+# Create a Dash application instance
 app = dash.Dash(__name__)
 
 # Define the layout
 app.layout = html.Div([
+    # Heading for the dashboard, displayed on the top of the page
     html.H1('Digital Wallet Transactions Dashboard'),
 
+    # Tabs for different visualizations
     dcc.Tabs([
+        # Each tab contains a different visualization
+        # First tab displays Locations Sorted by the Average Product Amount
         dcc.Tab(label='Top Locations', children=[
             dcc.Graph(
                 figure=go.Figure(
@@ -82,12 +149,13 @@ app.layout = html.Div([
                         )
                     ],
                     layout=go.Layout(
-                        title='Top 10 Locations with the Highest Average Product Amount',
+                        title='Locations Sorted by the Average Product Amount',
                         barmode='group'
                     )
                 )
             )
         ]),
+        # The second tab displays the sum of product amount by product category
         dcc.Tab(label='Product Categories', children=[
             dcc.Graph(
                 figure=go.Figure(
@@ -106,24 +174,19 @@ app.layout = html.Div([
                 )
             )
         ]),
+        # The third tab displays the Count of Different Device Usages in the Regions sorted by Average Sales
         dcc.Tab(label='Device Usage', children=[
             dcc.Graph(
                 figure=go.Figure(
-                    data=[
-                        go.Bar(
-                            x=df_query_3['location'],
-                            y=df_query_3['count_device_type_by_location'],
-                            name='Count of Device Type',
-                            marker=dict(color='red')
-                        )
-                    ],
+                    data=bars,
                     layout=go.Layout(
-                        title='Count of Different Device Usages in the 10 Regions with the Average Sales',
+                        title='Count of Different Device Usages in the Regions sorted by Average Sales',
                         barmode='group'
                     )
                 )
             )
         ]),
+        # The fourth tab displays the Top Ten Most Popular Merchant Names
         dcc.Tab(label='Merchant Popularity', children=[
             dcc.Graph(
                 figure=go.Figure(
@@ -142,26 +205,19 @@ app.layout = html.Div([
                 )
             )
         ]),
+        # The fifth tab displays the Payment Methods in the regions
         dcc.Tab(label='Payment Methods', children=[
             dcc.Graph(
                 figure=go.Figure(
-                    data=[
-                        go.Bar(
-                            x=df_query_5['location'],
-                            y=df_query_5['count_payment_method_by_location'],
-                            name='Count of Payment Method',
-                            marker=dict(color=colors),
-                            text = df_query_5['payment_method'],
-                            hoverinfo = 'text+y'
-                        )
-                    ],
+                    data=payment_method_bars,
                     layout=go.Layout(
-                        title='Payment Methods in least popular regions',
+                        title='Payment Methods in Regions',
                         barmode='group'
                     )
                 )
             )
         ]),
+        # The sixth tab displays the Regions with the Most Loyalty Points
         dcc.Tab(label='Loyalty Points', children=[
             dcc.Graph(
                 figure=go.Figure(
@@ -174,7 +230,7 @@ app.layout = html.Div([
                         )
                     ],
                     layout=go.Layout(
-                        title='Regions with the Most Loyalty Points',
+                        title='Regions sorted by the Most Loyalty Points',
                         barmode='group'
                     )
                 )
